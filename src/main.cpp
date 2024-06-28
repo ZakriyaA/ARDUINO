c48f39cc88a97cd8abafe30582956fdc04deb80d
Zakriya_Ash <ZAKRIYAASHRAF3@gmail.com>
2024-06-28 20:19:59 +1000

    ZAK

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <IRremote.h>
 
 // put function declarations here:
 int myFunction(int, int);
+int RECV_PIN = 11; // Define input pin on Arduino
+int SEND_PIN = 3;  // Define output pin for IR sender
+int buzzerPin = 9; // Pin connected to the passive buzzer
 
 void setup() {
+  Serial.begin(115200);   // Initialize serial communication
+  IrSender.begin(SEND_PIN); // Initialize IR sender on the specified pin
+  IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
